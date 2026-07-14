9efb7b7cdc7939f6c77fdfd588472302edb39059
jiyundeng888 <yuanyinzhichuang@163.com>
2026-07-14 18:21:21 +0800

    Fix slice-simulator page for static export compatibility

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -3,25 +3,20 @@
 import { SliceSimulator, getSlices } from "@prismicio/next";
 import { SliceZone } from "@prismicio/react";
 import { components } from "../../slices";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+import { useState, useEffect } from "react";
 
-function SliceSimulatorContent() {
-  const searchParams = useSearchParams();
-  const state = searchParams.get("state");
-  const slices = getSlices(state);
+export default function SliceSimulatorPage() {
+  const [slices, setSlices] = useState<any[]>([]);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const state = params.get("state");
+    setSlices(getSlices(state));
+  }, []);
 
   return (
     <SliceSimulator>
       <SliceZone slices={slices} components={components} />
     </SliceSimulator>
-  );
-}
-
-export default function SliceSimulatorPage() {
-  return (
-    <Suspense>
-      <SliceSimulatorContent />
-    </Suspense>
   );
 }
