fc94354e855d5fc69d16709f6034a685e83a7f3e
jiyundeng888 <yuanyinzhichuang@163.com>
2026-07-14 18:18:19 +0800

    Update src/slices/index.ts for Prismic live editing

---
+++ src/slices/index.ts
@@ -0,0 +1,11 @@
+import CategoryGrid from "./CategoryGrid";
+import FeaturesStrip from "./FeaturesStrip";
+import FeaturedProducts from "./FeaturedProducts";
+import Newsletter from "./Newsletter";
+
+export const components = {
+  category_grid: CategoryGrid,
+  features_strip: FeaturesStrip,
+  featured_products: FeaturedProducts,
+  newsletter: Newsletter,
+};
