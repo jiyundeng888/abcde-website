0eb950ba552a054f52f986c16d672d059ab8592a
jiyundeng888 <yuanyinzhichuang@163.com>
2026-07-15 10:36:31 +0800

    Prismic CMS integration: page.tsx

--- src/app/subwoofers/page.tsx
+++ src/app/subwoofers/page.tsx
@@ -1,16 +1,17 @@
 import SiteLayout from "../components/SiteLayout";
 import CategoryPage from "../components/CategoryPage";
-import { categoryData } from "../components/navConfig";
+import { getCategoryPageData } from "../lib/prismicData";
 
 export const metadata = {
   title: "Subwoofers - ABCDE Car Audio",
   description: "Shop ABCDE subwoofers - high-power handling subs for earth-shaking bass in your car.",
 };
 
-export default function SubwoofersPage() {
+export default async function SubwoofersPage() {
+  const data = await getCategoryPageData("subwoofers");
   return (
     <SiteLayout>
-      <CategoryPage data={categoryData.subwoofers} />
+      <CategoryPage data={data} />
     </SiteLayout>
   );
 }
