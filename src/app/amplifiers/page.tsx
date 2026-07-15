a2824820385499c4b78b62d6b0756ba5479cbfbe
jiyundeng888 <yuanyinzhichuang@163.com>
2026-07-15 10:36:24 +0800

    Prismic CMS integration: page.tsx

--- src/app/amplifiers/page.tsx
+++ src/app/amplifiers/page.tsx
@@ -1,16 +1,17 @@
 import SiteLayout from "../components/SiteLayout";
 import CategoryPage from "../components/CategoryPage";
-import { categoryData } from "../components/navConfig";
+import { getCategoryPageData } from "../lib/prismicData";
 
 export const metadata = {
   title: "Amplifiers - ABCDE Car Audio",
   description: "Shop ABCDE amplifiers - Class D monoblock and multi-channel amps for car audio systems.",
 };
 
-export default function AmplifiersPage() {
+export default async function AmplifiersPage() {
+  const data = await getCategoryPageData("amplifiers");
   return (
     <SiteLayout>
-      <CategoryPage data={categoryData.amplifiers} />
+      <CategoryPage data={data} />
     </SiteLayout>
   );
 }
